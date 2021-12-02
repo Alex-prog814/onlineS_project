45f27a7f49ddee9c7a378aabf67c9e33bdb994e0
alex <aleks.98.x.28@gmail.com>
2021-12-02 21:19:29 +0600

    Add filtration and search

--- onlineS_project/urls.py
+++ onlineS_project/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from onlineS_project import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('applications.product.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
