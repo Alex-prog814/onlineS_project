c5139fca6f109ec49b9508dbfe007f80c6a4e8be
alex <aleks.98.x.28@gmail.com>
2021-12-03 21:00:15 +0600

    Add registration and login

--- applications/user/views.py
+++ applications/user/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from applications.user.forms import RegistrationForm
+from applications.user.models import User
+
+
+class RegistrationView(CreateView):
+    model = User
+    form_class = RegistrationForm
+    template_name = 'register_page.html'
+    success_url = reverse_lazy('home-page')
